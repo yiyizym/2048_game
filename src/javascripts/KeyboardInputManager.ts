4494d4cb7bb04bcacf80c850d979c59fd57994ad
yiyizym <yiyizym@gmail.com>
2021-05-15 16:56:25 +0800

    revise KeyboardInputManager.ts with ts

--- src/javascripts/KeyboardInputManager.ts
+++ src/javascripts/KeyboardInputManager.ts
@@ -1,84 +1,83 @@
 import * as Hammer from 'hammerjs';
 
-function KeyboardInputManager() {
-    this.events = {};
+class KeyboardInputManager {
+  events: Map<string, any>;
 
+  constructor() {
     this.listen();
   }
 
-  KeyboardInputManager.prototype.on = function (event, callback) {
-    if (!this.events[event]) {
-      this.events[event] = [];
-    }
-    this.events[event].push(callback);
-  };
+  listen():void {
 
-  KeyboardInputManager.prototype.emit = function (event, data) {
-    var callbacks = this.events[event];
-    if (callbacks) {
-      callbacks.forEach(function (callback) {
-        callback(data);
-      });
-    }
-  };
-
-  KeyboardInputManager.prototype.listen = function () {
-    var self = this;
-
-    var map = {
-      38: 0, // Up
-      39: 1, // Right
-      40: 2, // Down
-      37: 3, // Left
-      75: 0, // vim keybindings
-      76: 1,
-      74: 2,
-      72: 3,
+    const map = {
+      'ArrowUp': 0,
+      'ArrowRight': 1,
+      'ArrowDown': 2,
+      'ArrowLeft': 3,
     };
 
-    document.addEventListener("keydown", function (event) {
-      var modifiers =
-        event.altKey || event.ctrlKey || event.metaKey || event.shiftKey;
-      var mapped = map[event.which];
+    document.addEventListener("keydown", (event: KeyboardEvent): void => {
+      const modifiers = event.altKey || event.ctrlKey || event.metaKey || event.shiftKey;
+      if(modifiers) { return }
 
-      if (!modifiers) {
-        if (mapped !== undefined) {
-          event.preventDefault();
-          self.emit("move", mapped);
-        }
+      const theKey = event.key
+      const mapped = map[theKey];
 
-        if (event.which === 32) self.restart.bind(self)(event);
+      if (mapped !== undefined) {
+        event.preventDefault();
+        this.emit("move", mapped);
       }
+
+      if (theKey === 'Space') this.restart(event);
     });
 
-    var retry = document.getElementsByClassName("retry-button")[0];
-    retry.addEventListener("click", this.restart.bind(this));
+    const retry = document.querySelector(".retry-button");
+    retry.addEventListener("click", this.restart);
+
+
 
     // Listen to swipe events
-    var gestures = [
+    const gestures = [
       Hammer.DIRECTION_UP,
       Hammer.DIRECTION_RIGHT,
       Hammer.DIRECTION_DOWN,
       Hammer.DIRECTION_LEFT,
     ];
 
-    var gameContainer = document.getElementsByClassName("game-container")[0];
-    var handler = Hammer(gameContainer, {
+    const gameContainer = document.querySelector(".game-container");
+    const handler = Hammer(gameContainer, {
       drag_block_horizontal: true,
       drag_block_vertical: true,
     });
 
-    handler.on("swipe", function (event) {
+    handler.on("swipe", (event) => {
       event.gesture.preventDefault();
       const mapped = gestures.indexOf(event.gesture.direction);
 
-      if (mapped !== -1) self.emit("move", mapped);
+      if (mapped !== -1) this.emit("move", mapped);
     });
-  };
+  }
 
-  KeyboardInputManager.prototype.restart = function (event) {
+  emit(event: string, data?: any):void {
+    const callbacks = this.events[event];
+    if(!callbacks) { return }
+    callbacks.forEach((callback: (data: any) => any): void => {
+      callback(data);
+    });
+  }
+
+  on = (event: string, callback: (args: any) => any): void => {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(callback);
+  }
+
+  restart = (event: Event): void => {
     event.preventDefault();
     this.emit("restart");
-  };
+  }
 
-  export default KeyboardInputManager;
+}
+
+export default KeyboardInputManager;
